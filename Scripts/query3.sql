5fffa11e0c2fa8333d150d5f610f7706a3cd2ced
paulie <palsanjay244@gmail.com>
2024-06-19 16:16:01 +0000

    commit

--- Scripts/query3.sql
+++ Scripts/query3.sql
@@ -1,1 +1,8 @@
-d
+SELECT e.employeeId,e.firstName,e.lastName,count(s.salesId) as car_count,
+max(s.salesAmount) AS max,min(s.salesAmount) as min
+FROM employee e
+LEFT JOIN sales s
+ON e.employeeId = s.employeeId
+GROUP BY e.employeeId,e.firstName,e.lastName
+HAVING count(s.salesId) > 5
+ORDER by car_count DESC
